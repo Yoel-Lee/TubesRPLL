d6c6abddf1e988e87081eabf46ae34519ba8abb0
Yoel-Lee <yoelgame456@gmail.com>
2026-04-28 22:40:03 +0700

    backend : database & Api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,45 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-// load env dari .env
-dotenv.config();
+import { PrismaClient } from '@prisma/client';
 
 
 const app = express();
+const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
-
+// Middleware
 app.use(cors()); 
 app.use(express.json()); 
 
-//  test
+
 app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Selamat datang di API HRIS!',
-    status: 'Server berjalan dengan baik',
+  res.status(200).json({ 
+    message: 'Server nyala' 
   });
 });
 
-// simulasi dummy
-app.get('/api/users', (req: Request, res: Response) => {
-  const dummyUsers = [
-    { id: 1, name: 'Pak Bos Admin', role: 'admin' },
-    { id: 2, name: 'Budi Santoso', role: 'staff' }
-  ];
-  res.json(dummyUsers);
+// 2. test endpont api
+app.get('/api/test-db', async (req: Request, res: Response) => {
+  try {
+    
+    const users = await prisma.user.findMany();
+    //masih kosong , tapi sekedar test aja berhasil ga
+    res.status(200).json({
+      message: 'Koneksi ke PostgreSQL gacor!',
+      totalUsers: users.length,
+      data: users
+    });
+  } catch (error) {
+    console.error('Error Database:', error);
+    res.status(500).json({ 
+      message: 'Gagal terhubung ke database. Cek DATABASE_URL di file .env', 
+      error: String(error) 
+    });
+  }
 });
 
-// server
+// Jalankan Server
 app.listen(PORT, () => {
-  console.log(`🚀 Server Backend berjalan di http://localhost:${PORT}`);
+  console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
